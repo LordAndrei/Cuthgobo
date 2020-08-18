5242c761a30f14c40de8d419c4f20efde1da5df6
Andrei Freeman <andrei@lordandrei.com>
2020-08-18 15:26:16 -0400

    Initial Wothcocu ideas

--- Cuthgobo/Wothcocu.swift
+++ Cuthgobo/Wothcocu.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit // For CGPoint
+
+enum WorldItems {
+    case cuthgobo
+    case focumu
+    case wacusi
+    case empty
+}
+
+class Wothcocu {
+    let minSize = 0
+    let maxSize = 10
+    var turn = 0
+    
+    var beings: [Cuthgobo] = []
+    var population: [NSIndexPath: [WorldItems]] = [:]
+    // Focumu: Food Cuthgobo Munch
+    // Wacusi: Water Cuthgono Sip
+    
+    func beBorn() {
+        createWater()
+        for _ in 1...2 {
+            growNewFood()
+        }
+        createBeings()
+    }
+    
+    func newTurn() {
+        for being in beings {
+            being.doSomething()
+        }
+    }
+    
+    func createWater() {}
+    func growNewFood() {}
+    func createBeings() {}
+}
+
