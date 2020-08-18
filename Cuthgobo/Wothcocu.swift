9f1b3246ad6d9788fbba3eb2fb70fd389ba3dd41
Andrei Freeman <andrei@lordandrei.com>
2020-08-18 16:36:12 -0400

    Make WorldItems public set initial being count moar foodz, nom new turn increments turn counter and prints status no world prints itself createBeings generates cuthgobo and hangs them in world storage print world was a pain in the butt.

--- Cuthgobo/Wothcocu.swift
+++ Cuthgobo/Wothcocu.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit // For CGPoint
 
-enum WorldItems {
+public enum WorldItems {
     case cuthgobo
     case focumu
     case wacusi
@@ -19,6 +19,7 @@ enum WorldItems {
 class Wothcocu {
     let minSize = 0
     let maxSize = 10
+    let initialBeings = 5
     var turn = 0
     
     var beings: [Cuthgobo] = []
@@ -28,13 +29,16 @@ class Wothcocu {
     
     func beBorn() {
         createWater()
-        for _ in 1...2 {
+        for _ in 1...10 {
             growNewFood()
         }
         createBeings()
+        printWorld()
     }
     
     func newTurn() {
+        turn += 1
+        print("Wothcocu Day: \(turn)")
         for being in beings {
             being.doSomething()
         }
@@ -42,6 +46,68 @@ class Wothcocu {
     
     func createWater() {}
     func growNewFood() {}
-    func createBeings() {}
+    
+    func createBeings() {
+        for _ in 1...initialBeings {
+            let (x,y) = getLocation()
+            let aLocation = CGPoint(x: x, y: y)
+            let anIndexPath = NSIndexPath(row: x, section: y)
+            let aCuthgobo = Cuthgobo.init()
+            aCuthgobo.beBorn(whereBorn: aLocation)
+            population[anIndexPath] = [.cuthgobo]
+        }
+    }
+    
+    func getLocation() -> (Int, Int) {
+        let x = Int(arc4random()) % maxSize
+        let y = Int(arc4random()) % maxSize
+        return (x,y)
+    }
+    
+    func printWorld() {
+        for y in minSize..<maxSize {
+            var printString: [String] = ["","","","",""]
+            for x in minSize..<maxSize {
+                let anIndexPath = NSIndexPath(row: x, section: y)
+                let localPopulation = population[anIndexPath]
+                printString[0] += "+-------"
+                printString[1] += "|       "
+                printString[2] += "| "
+                printString[3] += "| (\(x),\(y)) "
+                if localPopulation != nil {
+                    if localPopulation!.contains(.cuthgobo) {
+                        printString[2] += "C "
+                    } else {
+                        printString[2] += "  "
+                    }
+                    if localPopulation!.contains(.focumu) {
+                        printString[2] += "F "
+                    } else {
+                        printString[2] += "  "
+                    }
+                    if localPopulation!.contains(.wacusi) {
+                        printString[2] += "W "
+                    } else {
+                        printString[2] += "  "
+                    }
+                } else {
+                    printString[2] += "      "
+                }
+            }
+            printString[0] += "+"
+            printString[1] += "|"
+            printString[2] += "|"
+            printString[3] += "|"
+            for index in 0...3 {
+                print(printString[index])
+            }
+        }
+        var baseString = ""
+        for _ in 0..<maxSize {
+            baseString += "+-------"
+        }
+        baseString += "+"
+        print(baseString)
+    }
 }
 
