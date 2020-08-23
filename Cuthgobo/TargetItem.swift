01c78d063b22219078f393b3e85485d5f13884d6
Andrei Freeman <andrei@lordandrei.com>
2020-08-22 21:27:00 -0400

    Add TargetItem structure

---
+++ Cuthgobo/TargetItem.swift
@@ -0,0 +1,14 @@
+//
+//  TargetItem.swift
+//  Cuthgobo
+//
+//  Created by Andrei Freeman on 8/22/20.
+//
+
+import Foundation
+import UIKit
+
+struct TargetItem {
+    var direction: CGPoint
+    var distance: Float
+}
